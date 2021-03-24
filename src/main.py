3f4a9dc8f68419363be8c2a27138e471ca59636d
Jason Northcote <jason.northcote@gmail.com>
2021-03-24 08:33:57 -0700

    Its 8 33

--- src/main.py
+++ src/main.py
@@ -12,5 +12,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print(args.challenge)
     cClass = entity_factory(args.challenge)
-    challenge = cClass(len(args.array), args.array)
+    challenge = cClass(args.array)
     challenge.evaluate()
